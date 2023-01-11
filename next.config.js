7e88c94a5a428b1c59f9d7b34a985e7ab8e11650
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2023-01-10 21:59:34 -0600

    Fixed bug issues created by WordPress by making request in the client-side

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ module.exports = {
 		NEXT_PUBLIC_PRODUCTION: process.env.PRODUCTION, // True when in public domain and False when in local domain
 		NEXT_PUBLIC_KEVINFONSECA_URL: process.env.KEVINFONSECA_URL,
 		NEXT_PUBLIC_KEVINFONSECA_API_URL: process.env.KEVINFONSECA_API_URL,
-		// NEXT_PUBLIC_API_URL: "https://befree.herokuapp.com",
-		// NEXT_PUBLIC_FRONTEND_URL: "https://ytdownloader-phi.vercel.app",
-		NEXT_PUBLIC_API_URL: "http://localhost:5000",
-		NEXT_PUBLIC_FRONTEND_URL: "http://localhost:3000",
+		NEXT_PUBLIC_API_URL: "https://befree.herokuapp.com",
+		NEXT_PUBLIC_FRONTEND_URL: "https://ytdownloader-phi.vercel.app",
+		// NEXT_PUBLIC_API_URL: "http://localhost:5000",
+		// NEXT_PUBLIC_FRONTEND_URL: "http://localhost:3000",
 	},
 	images: {
 		domains: [
