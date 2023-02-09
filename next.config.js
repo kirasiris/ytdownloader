791ad609e5fa56630a4bd506f049e6a6840b3093
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2023-02-09 01:05:06 -0600

    ...

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ module.exports = {
 		NEXT_PUBLIC_WEBSITE_NAME_HTTP_STRING: process.env.WEBSITE_NAME_HTTP_STRING,
 		NEXT_PUBLIC_KEVINFONSECA_URL: process.env.KEVINFONSECA_URL,
 		NEXT_PUBLIC_KEVINFONSECA_API_URL: process.env.KEVINFONSECA_API_URL,
-		// NEXT_PUBLIC_API_URL: "https://befree.herokuapp.com",
-		// NEXT_PUBLIC_FRONTEND_URL: "https://ytdownloader-phi.vercel.app",
-		NEXT_PUBLIC_API_URL: "http://localhost:5000",
-		NEXT_PUBLIC_FRONTEND_URL: "http://localhost:3000",
+		NEXT_PUBLIC_API_URL: "https://befree.herokuapp.com",
+		NEXT_PUBLIC_FRONTEND_URL: "https://ytdownloader-phi.vercel.app",
+		// NEXT_PUBLIC_API_URL: "http://localhost:5000",
+		// NEXT_PUBLIC_FRONTEND_URL: "http://localhost:3000",
 	},
 	images: {
 		domains: [
